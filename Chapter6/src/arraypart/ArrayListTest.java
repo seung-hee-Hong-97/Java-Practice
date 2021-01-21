db8d016489621a4c1f994b8e1f696193de22df9d
sejong77 <68320595+sejong77@users.noreply.github.com>
2021-01-21 22:24:11 +0900

    ArrayList, TowDimensionArray

---
+++ Chapter6/src/arraypart/ArrayListTest.java
@@ -0,0 +1,26 @@
+package arraypart;
+
+import java.util.ArrayList;
+
+public class ArrayListTest {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> list= new ArrayList<String>();
+		
+		list.add("aaa");
+		list.add("bbb");
+		list.add("ccc");
+		
+		/*for(String s: list) {
+			System.out.println(s);
+		} */
+		
+		for(int i=0; i<list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		
+		
+	}
+
+}
